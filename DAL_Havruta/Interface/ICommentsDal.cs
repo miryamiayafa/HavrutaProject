b213e1bb4eb570adaaf84eee9dee2e8ae6cecdc3
miryami <miryami@DESKTOP-54MI0R7>
2023-12-20 18:23:11 +0200

    Add project files.

---
+++ DAL_Havruta/Interface/ICommentsDal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DAL_Havruta.Interfase 
+{
+     public interface ICommentsDal
+     {
+	
+     }
+}
+
+    
---
+++ DAL_Havruta/Interface/ISubjectDal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DAL_Havruta.Interfase
+{
+    public interface ISubjectDal
+    {
+
+    }
+}
+

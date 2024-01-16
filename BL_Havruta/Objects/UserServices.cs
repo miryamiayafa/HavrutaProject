dd39427bd554296d32e7a77e2a77b65904f18557
miryamiayafa <miryam7.52002@gmail.com>
2024-01-16 16:50:26 +0200

    i did MappingProfile and ServicesConfig in the BL

--- BL_Havruta/Objects/UserServices.cs
+++ BL_Havruta/Objects/UserServices.cs
@@ -1,38 +1,16 @@
-﻿using System;
+﻿using AutoMapper;
+using DAL_Havruta.Interfase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BL_Havruta.Interface;
-using DTO_Havruta.Model;
 
 namespace BL_Havruta.Objects
 {
-    internal class UserServices : IUserServices
+    public class UserServices : IUserServices
     {
-        public bool AddNew(User newUser)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Delete(User deleteUser)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<User> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public User GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public User GetByUser(string emaile)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }

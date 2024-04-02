3a850e0ebdb484438b2a44be07ef38e086c7b3cc
miryamiayafa <miryam7.52002@gmail.com>
2024-04-02 10:26:01 +0300

    2/4/24

---
+++ HavrutaAPI/Controllers/TimeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HavrutaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+    }
+}
